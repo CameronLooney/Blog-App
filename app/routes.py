73f900dc0bf8f0a9b435c72e6c974131dc09b442
Cameron <cameronlooney18@gmail.com>
2022-06-07 23:45:08 +0100

    added login validation

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 # View functions are mapped to one or more route URLs so that Flask knows what logic to execute when a client requests a given URL.
 
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
 from app.form import LoginForm
 
 
@@ -27,9 +27,15 @@ def index():
     return render_template('index.html', user = user, posts = posts)
 
 # new view for login page
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST']) # accept both get and post
 def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    form = LoginForm() # generate login object
+    if form.validate_on_submit():
+        # if the form is okay then we will redirect to home page
+        # if the data is not valid we render the login template again
 
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
