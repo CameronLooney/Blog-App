b6c7d934b472f8df1172ee0445f0cdcf4ac58601
Cameron <cameronlooney18@gmail.com>
2022-06-08 20:57:45 +0100

    improved link/ url handling

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@
 # View functions are mapped to one or more route URLs so that Flask knows what logic to execute when a client requests a given URL.
 
 from app import app
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.form import LoginForm
 
 
@@ -36,6 +36,6 @@ def login():
 
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for("index"))
     return render_template('login.html', title='Sign In', form=form)
 
