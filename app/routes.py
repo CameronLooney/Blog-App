c550d2f5161c59073e3818becca3fd265049ecfe
Cameron <cameronlooney18@gmail.com>
2022-06-10 10:28:20 +0100

    added logout button

--- app/routes.py
+++ app/routes.py
@@ -49,3 +49,9 @@ def login():
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
 
+from flask_login import logout_user
+# uses built in logout function ,redirect the user to home page
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
